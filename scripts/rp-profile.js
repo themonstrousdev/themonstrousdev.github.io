6f39d4ee41bcdaeca7ed4c4da82e1612915aa7ca
Monster Dev <ellexidecodes@gmail.com>
2019-04-13 21:46:45 +0800

    push

---
+++ scripts/rp-profile.js
@@ -0,0 +1,62 @@
+function showRP() {
+  fetch("https://www.themonster.xyz/src/rp-profile.html")
+    .then(res => {
+      return res.text()
+    })
+    .then(text => {
+      var cut = text.search("/script") + 8,
+      html = text.slice(cut);
+
+      $("<div>", {
+        id: "background",
+        html: html
+      }).insertBefore("#choose");
+
+      document.getElementById("exitProfile").onclick = function() {
+        removeRP()
+      }
+
+      setTimeout(function(){
+        $("#choose").fadeOut(200);
+      }, 200)
+    })
+}
+
+function removeRP() {
+  $("#background").fadeOut(200);
+  setTimeout(() => {
+    $("#background").remove();
+  }, 200);
+}
+
+$(document).ready(()=>{
+  $("<div>", {
+    id: "choose",
+    html: `
+      <div class="texture"></div>
+      <div id="chooseProfile">
+        <div class="choice__prof" id="coding">Coding Profile</div>
+        <div class="choice__prof" id="rp">Character Profile</div>
+      </div>
+    `
+  }).insertBefore("#loading");
+
+  $("#coding.choice__prof").click(()=>{
+    $("#choose").fadeOut(200);
+    setTimeout(function(){
+      $("#choose").remove();
+    }, 200);
+  });
+
+  $("#rp.choice__prof").click(()=>{
+    showRP();
+    $("#chooseProfile").fadeOut(200);
+    setTimeout(function(){
+      $("#chooseProfile").remove();
+    }, 200);
+  })
+
+  $("#showRP").click(()=>{
+    showRP();
+  })
+})
