f6c06ce7a585b212d7bd1ec790a29fecb114c060
Monster Dev <ellexidecodes@gmail.com>
2019-04-19 19:18:43 +0800

    I forgot that you got redirected to thanks but eh

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,94 +1,118 @@
-fetch("https://getsimpleform.com/messages.json?api_token=c3d5d9499780701b4ce87450444c2390")
+var previousData = null;
+
+function processComments(data) {
+  var i;
+  if(!previousData) {
+    previousData = data;
+    appendComments(previousData);
+  } else if(previousData == data) {
+    // do nothing
+  } else if (previousData != data) {
+    $("#comments").html(""); 
+    previousData = data;
+    appendComments(previousData);
+  }
+}
+
+function appendComments(data) {
+  if (data.length == 0) {
+    $("#feedback #comments").append("<h2 style='padding: 0; text-align: center; font-family: Hemera, sans-serif; text-transform: lowercase;font-size: 2vw;'>No one has left a feedback yet! Be the first one.</h2>");
+  } else {
+    for(i = 0; i < data.length; i++) {
+      var date = moment(data[i]["created_at"]).format("LLL"),
+      sender = data[i]["data"].sender,
+      message = data[i]["data"].message;
+      
+      $("<div>", {
+        class: "commentContainer"
+      }).prependTo("#comments");
+
+      $("<div>", {
+        class: "sender",
+        html: `${sender == ""?"Anonymous":sender} <span>${date}</span>`
+      }).appendTo("#comments .commentContainer:first-child");
+
+      $("<div>", {
+        class: "message",
+        html: message
+      }).appendTo("#comments .commentContainer:first-child");
+    }
+  }
+
+  var formPos = $("form").offset(),
+    scrollerPos = $(".scroller").offset(),
+    scrollInner = $(".scroller").innerHeight();
+
+    console.log(formPos.top > scrollerPos.top+scrollInner);
+
+    function scrollToFeedback(scroll) {
+      $(".scroller").animate({
+        scrollTop: scroll
+      }, 500, "swing");
+    }
+
+    if(formPos.top > scrollerPos.top+scrollInner) {
+      $("#content").append($("<div>", {
+        class: "toFeedback",
+        html: "Give Feedback",
+        click: function() {
+          scrollToFeedback(formPos.top);
+        }
+      }));
+    }
+
+    $(".scroller").scroll(function(){
+      var btn = $("#content").find(".toFeedback").length;
+      formPos = $("form").offset();
+      if(formPos.top > scrollerPos.top+scrollInner) {
+        if(btn == 0) {
+          $("#content").append($("<div>", {
+            class: "toFeedback",
+            html: "Give Feedback",
+            click: function() {
+              scrollToFeedback(formPos.top);
+            }
+          }));
+        } 
+      } else {
+        if(btn > 0) {
+          $(".toFeedback").animate({
+            "margin-bottom": "-10vh"
+          }, 200, ()=>{
+            $(".toFeedback").remove();
+          });
+        }
+      }
+    }); 
+}
+
+function fetchData() {
+  fetch("https://getsimpleform.com/messages.json?api_token=c3d5d9499780701b4ce87450444c2390")
   .then(res => {
     return res.json()
   })
   .then(data => {
-    var i;
-    if (data.length == 0) {
-      $("#feedback #comments").append("<h2 style='padding: 0; text-align: center; font-family: Hemera, sans-serif; text-transform: lowercase;font-size: 2vw;'>No one has left a feedback yet! Be the first one.</h2>");
-    } else {
-      for(i = 0; i < data.length; i++) {
-        var date = moment(data[i]["created_at"]).format("LLL"),
-        sender = data[i]["data"].sender,
-        message = data[i]["data"].message;
-        
-        $("<div>", {
-          class: "commentContainer"
-        }).prependTo("#comments");
-
-        $("<div>", {
-          class: "sender",
-          html: `${sender == ""?"Anonymous":sender} <span>${date}</span>`
-        }).appendTo("#comments .commentContainer:first-child");
-
-        $("<div>", {
-          class: "message",
-          html: message
-        }).appendTo("#comments .commentContainer:first-child");
-      }
-    }
-
-    var formPos = $("form").offset(),
-      scrollerPos = $(".scroller").offset(),
-      scrollInner = $(".scroller").innerHeight();
-
-      console.log(formPos.top > scrollerPos.top+scrollInner);
-
-      function scrollToFeedback(scroll) {
-        $(".scroller").animate({
-          scrollTop: scroll
-        }, 500, "swing");
-      }
-
-      if(formPos.top > scrollerPos.top+scrollInner) {
-        $("#content").append($("<div>", {
-          class: "toFeedback",
-          html: "Give Feedback",
-          click: function() {
-            scrollToFeedback(formPos.top);
-          }
-        }));
-      }
-
-      $(".scroller").scroll(function(){
-        var btn = $("#content").find(".toFeedback").length;
-        formPos = $("form").offset();
-        if(formPos.top > scrollerPos.top+scrollInner) {
-          if(btn == 0) {
-            $("#content").append($("<div>", {
-              class: "toFeedback",
-              html: "Give Feedback",
-              click: function() {
-                scrollToFeedback(formPos.top);
-              }
-            }));
-          } 
-        } else {
-          if(btn > 0) {
-            $(".toFeedback").animate({
-              "margin-bottom": "-10vh"
-            }, 200, ()=>{
-              $(".toFeedback").remove();
-            });
-          }
-        }
-      });
-
+    processComments(data);
     return data
   })
   .then(res => {
     var container = $(".scrollerWrap.body.notLoaded");
-    container.waitForImages(()=>{
-      container.animate({
-        "opacity": "1",
-      }, 200);
-      setTimeout(() => {
-        container.css("pointer-events", "all");
-      }, 200);
+    container.animate({
+      "opacity": "1"
+    }, 200);
+    setTimeout(() => {
+      container.css("pointer-events", "all");
+    }, 200);
       loaded = true;
-    })
-      endLoad();
+    endLoad();
   })
   .catch(err => {
     logError(err);
-  })
+  });
+};
+
+fetchData();
+
+$("#feedback button[type=submit]").click(()=>{
+  fetchData();
+});
