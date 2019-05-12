9d6389dff9be7836685b4773aa0c86216ad60230
Monster Dev <ellexidecodes@gmail.com>
2019-05-12 19:56:47 +0800

    save current title

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-var body = {url: "./src/home.html"}, loaded = false, loader, prevPage = [], prevScript = [], content = $("#content"),
-currentTab = content.attr("open-tab"), orient, momentjs = document.createElement("script"),date = new Date(), currentYear = date.getFullYear(), prevHeads = [], header = document.title;
+var body = {url: "./src/home.html", title:document.title}, loaded = false, loader, prevPage = [], prevScript = [], content = $("#content"),
+currentTab = content.attr("open-tab"), orient, momentjs = document.createElement("script"),date = new Date(), currentYear = date.getFullYear(), prevHeads = [];
 
 $.getScript('./scripts/libs/swiped-events-master/src/swiped-events.js', function() {
   logSuccess("Injected swipe detector");
