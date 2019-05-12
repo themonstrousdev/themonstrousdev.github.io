801ac89b187871d6b22bd539e8b78c9d4246bf37
Monster Dev <ellexidecodes@gmail.com>
2019-05-12 21:54:09 +0800

    take the title bitch

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
-var body = {url: "./src/home.html", title:document.title}, loaded = false, loader, prevPage = [], prevScript = [], content = $("#content"),
+var body = {url: "./src/home.html"}, loaded = false, loader, prevPage = [], prevScript = [], content = $("#content"),
 currentTab = content.attr("open-tab"), orient, momentjs = document.createElement("script"),date = new Date(), currentYear = date.getFullYear(), prevHeads = [];
+
+body.title = document.title;
 
 $.getScript('./scripts/libs/swiped-events-master/src/swiped-events.js', function() {
   logSuccess("Injected swipe detector");
